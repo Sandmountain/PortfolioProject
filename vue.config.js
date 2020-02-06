94cded5dddf4c3be1567c7af6470820acc81f640
Sandmountain <sandtain@gmail.com>
2020-02-06 11:01:07 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? "/PortfolioProject/" : "/"
+  //publicPath: process.env.NODE_ENV === "production" ? "/PortfolioProject/" : "/"
 };
