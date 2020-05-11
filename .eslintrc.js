35c936e220d90e12a631c5e257fc6504e5348295
viktor.sandberg <viktor.sandberg@angrycreative.se>
2020-05-11 21:27:50 +0200

    Worked alot with portfolio part of the page

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
 module.exports = {
   extends: [
     // add more generic rulesets here, such as:
-    // 'eslint:recommended',
-    "plugin:vue/recommended"
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended'
   ],
   rules: {
     // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    'vue/no-unused-vars': 'warn',
+    'no-unused-vars': 'warn',
+    'vue/component-name-in-template-casing': ['warn', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 };
